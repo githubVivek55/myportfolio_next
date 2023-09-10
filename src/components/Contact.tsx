3b902f96983ca1719cdb9e2dc21450e9e66d7ebb
githubVivek55 <vivekpce55@outlook.com>
2023-09-10 15:30:25 +0800

    add btn hover effect

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -78,7 +78,7 @@ const Contact = () => {
             />
           </label>
           <button
-            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+            className='hover:cursor-pointer hover:bg-[#667eea] bg-[#915eff] py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
             type='submit'
             disabled={!formState.isValid}
           >
